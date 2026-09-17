040fb6583b91f126b62522d195843535ee834001
VanshitSinghChauhan <vanshitsinghchauhan16032005@gmail.com>
2026-09-17 13:59:23 +0530

    Connected Server and imported cors, cookie-parser

--- src/db/index.js
+++ src/db/index.js
@@ -15,3 +15,26 @@ const connectDB = async () => {
     }
 };
 export default connectDB;
+
+//Another method to connect mongo db is use this code directly in main index.js// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants.js";
+// import express from "express";
+
+// const app = express()
+/*
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on("error", (error) => {
+            console.log("Error: ", error);
+            throw error;
+        });
+        app.listen(process.env.PORT, () => {
+            console.log(`Express is listening on port ${process.env.PORT}`);
+        });
+    } catch (error) {
+        console.log("Error: ", error);
+        throw error;
+    }
+})();
+*/
--- src/index.js
+++ src/index.js
@@ -1,31 +1,18 @@
-// require('dotenv').config({path:'./env'})
+// require('dotenv').config({path:'./env'})<--this will also work
 import dotenv from "dotenv";
-// import mongoose from "mongoose";
-// import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./env",
 });
 
-connectDB();
-// import express from "express";
-
-// const app = express()
-/*
-(async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        app.on("error", (error) => {
-            console.log("Error: ", error);
-            throw error;
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
         });
-        app.listen(process.env.PORT, () => {
-            console.log(`Express is listening on port ${process.env.PORT}`);
-        });
-    } catch (error) {
-        console.log("Error: ", error);
-        throw error;
-    }
-})();
-*/
+    })
+    .catch((err) => {
+        console.log("MongoDB Connection Failed!!!", err);
+    });
